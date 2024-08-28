b2fc7dd7602cdf1cac1c4af22b4a41527da9b80c
crownaintanoob <70343800+crownaintanoob@users.noreply.github.com>
2024-08-28 19:49:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,35 +12,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    usage = 'Pass a properly encoded url parameter e.g. /https/www.google.com'
+    usage = 'erm what the sigma!'
     return usage
 
-@app.route('/https/<url>')
+@app.route('/fetch/<url>')
 def root(url):    
     url = 'https://' + url
-    r = requests.get(url)
+    r = requests.get(url, proxies={
+        "http": "http://qywiproxy-rotate:qywiErmWhatTheSigma6959@p.webshare.io:80/",
+        "https": "http://qywiproxy-rotate:qywiErmWhatTheSigma6959@p.webshare.io:80/"
+    }))
     rr = Response(response=r.content, status=r.status_code)
-    rr.headers["Content-Type"] = r.headers['Content-Type']
-    return rr
-
-@app.route('/g/<keyword>')
-def gkeyword(keyword):    
-    url = 'https://www.google.com/search'
-    payload = {'q':keyword, 'num':1, 'start':1, 'sourceid':'chrome', 'ie':'UTF-8', 'cr':'cr=countryUS'}
-    r = requests.get(url, params=payload)
-    rr = Response(response=r.content, status=r.status_code)
-    rr.headers["Content-Type"] = r.headers['Content-Type']
-    return rr
-
-@app.route('/r/<subreddit>/subscribers')
-def gsubreddit(subreddit):
-    url = 'https://old.reddit.com/r/' + subreddit
-    xpath ="//span[@class='subscribers']/span[@class='number']/text()"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    r = requests.get(url, headers=headers)
-    tree = html.fromstring(r.content)
-    subscribers = tree.xpath(xpath)
-    rr = Response(response=subscribers, status=r.status_code)
     rr.headers["Content-Type"] = r.headers['Content-Type']
     return rr
 
