2679541fe1f7ce96733a8e340f41b026bf83f954
crownaintanoob <70343800+crownaintanoob@users.noreply.github.com>
2024-08-28 20:08:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def root(url):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 8000.
     port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
