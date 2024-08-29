a8897ab85af8ae9c10a690a5195afa464dab2c86
crownaintanoob <70343800+crownaintanoob@users.noreply.github.com>
2024-08-28 20:03:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def root(url):
     r = requests.get(url, proxies={
         "http": "http://qywiproxy-rotate:qywiErmWhatTheSigma6959@p.webshare.io:80/",
         "https": "http://qywiproxy-rotate:qywiErmWhatTheSigma6959@p.webshare.io:80/"
-    }))
+    })
     rr = Response(response=r.content, status=r.status_code)
     rr.headers["Content-Type"] = r.headers['Content-Type']
     return rr
